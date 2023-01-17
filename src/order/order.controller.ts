ea15e8349be27e8234c58191d8e027ace11d54b0
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2023-01-17 12:54:08 +0100

    guard fix

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -12,29 +12,27 @@ import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { AuthGuard } from '@nestjs/passport';
-
+import { Request } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post(':id')
-  @UseGuards(AuthGuard('jwt'))
-  async create(
-    @Param('id') userId: number,
-    @Body() createOrderDto: CreateOrderDto,
-  ) {
-    return this.orderService.create(userId, createOrderDto);
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() createOrderDto: CreateOrderDto, @Request() req) {
+    return this.orderService.create(req.user.id, createOrderDto);
   }
 
   @Patch(':id')
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   async update(
     @Param('id') id: number,
     @Body() updateOrderDto: UpdateOrderDto,
   ) {
     return this.orderService.update(id, updateOrderDto);
   }
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: number) {
     return this.orderService.remove(id);
